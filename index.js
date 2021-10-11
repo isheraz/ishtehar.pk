51532b3164d6db03bbc8c9e8b48001acf399c396
Sheraz Ahmed <sherazahmed93@gmail.com>
2021-10-11 13:01:12 +0500

    Requested Changes

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const path = require("path");
 const db = require("./config/database");
 
 // Test DB
+const init = async () => {
 
-db.authenticate()
-  .then(() => console.log("Database COnnected"))
-  .catch((err) => console.log("Error: " + err));
+try{
+  const connectionResp = await db.authenticate();
 
-const app = express();
+  const app = express();
 
-app.get("/", (req, res) => res.send("INDEX"));
+  app.get("/", (req, res) => res.send("INDEX"));
 
-//User Routes
+  //User Routes
 
-app.use("/user", require("./routes/user"));
+  app.use("/user", require("./routes/user"));
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+  app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+}catch (err) {
+
+}
+}
+init();
